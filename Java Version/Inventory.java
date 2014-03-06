2af32436ac82875729e68e3fae9849c4bf75bb8d
JozefStodulski <jozefstodulski@Jozefs-MacBook-Pro.local>
2014-03-06 11:53:21 +0000

    inventory...

--- Java Version/Inventory.java
+++ Java Version/Inventory.java
@@ -1,22 +1,66 @@
+import java.util.Set;
+import java.util.HashMap;
 import java.util.ArrayList;
+/**
+ * Write a description of class inventory here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
 
 public class Inventory
 {
-	private ArrayList<Object> objects;
+    // instance variables - replace the example below with your own
+    private ArrayList<Item> playerInventory;
+    private int inventoryWeight = 0;
+    private int inventoryWeightCapacity = 10;
+    
+    /**
+     * Constructor for objects of class inventory
+     */
+    public Inventory()
+    {
+        playerInventory = new ArrayList<Item>();
+    }
+    
+    public void addItemToInventory(Item item)
+    {
+        if (inventoryWeight() <= inventoryWeightCapacity)
+        {
+            playerInventory.add(item);
+            System.out.println(item + " has been put in your bag.");
+        } else {
+            System.out.println("Your bag's too heavy already. Try dropping something before picking this up.");
+        }
+    }
+    
+    public void dropFromInventory(int index)
+    {
+        playerInventory.remove(index);
+    }
+    
+    public String currentInventory(){
 
-	public Inventory()
-	{
-		objects = new ArrayList<Object>();
-	}
-
-	public void addObject(Object object)
-	{
-		objects.add(object);
-	}
-
-	public void removeObect(int index)
-	{
-		// remember to add validation on the index
-		objects.remove(index);
-	}
+        String returntxt = "You currently have: \n";
+        for (Item item : playerInventory)
+        {
+            returntxt += "  - " + item.getDescription() + "\n";
+        }
+        return returntxt;
+    }
+    
+    public int inventorySize()
+    {
+        return playerInventory.size();
+    }
+    
+    public int inventoryWeight()
+    {
+        for (Item item: playerInventory)
+        {
+            inventoryWeight += item.getWeight();
+        }
+        return inventoryWeight;
+    }
 }
