8ba5296e58bbad0409660d153fcec1c3c020a1da
Liam Mann <liam@liammann.com>
2014-03-06 13:13:42 +0000

    Combat Started Outline

---
+++ Java Version/Combat.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Combat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Combat
+{
+    // instance variables - replace the example below with your own
+    private Character player;
+    private Character enemy;
+
+    /**
+     * Constructor for objects of class Combat
+     */
+    public Combat(Character player, Character enemy)
+    {
+        this.player = player;
+        this.enemy = enemy;
+        // initialise instance variables
+
+    }
+
+    /**
+     * 
+     * If you choose to defend you should lose less health on the next attack 
+     * each person takes a turn 
+     * strength and health should change the outcome of the fight
+     * the bot and player get a choice of attacking or defending
+     * get to choose what weapon to attack with.... 
+     * 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+
+    }
+}
