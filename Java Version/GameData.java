d8a5a5ebb2ff35de93090d2aa07379d728f0864a
Thomas Punt <tpunt@hotmail.co.uk>
2014-03-07 22:47:37 +0000

    Fixed a bug in backtracking
    Had to ensure that backtracking did not go beyond the start of the
    ArrayList

--- Java Version/GameData.java
+++ Java Version/GameData.java
@@ -2,27 +2,33 @@ import java.util.ArrayList;
 
 public class GameData
 {
-	private static ArrayList<Location> locationHistory;
+    private static ArrayList<Location> locationHistory;
 
-	public GameData()
-	{
-		locationHistory = new ArrayList<Location>();
-	}
+    public GameData()
+    {
+        locationHistory = new ArrayList<Location>();
+    }
 
-	public Location getCurrentLocation()
-	{
-		return locationHistory.get(locationHistory.size()-1);
-	}
+    public Location getCurrentLocation()
+    {
+        return locationHistory.get(locationHistory.size()-1);
+    }
 
-	public String setNewLocation(Location location)
-	{
-		locationHistory.add(location);
-		return ""; // output what the new location has been set to? means changing game class (welcome() method)
-	}
+    public String setNewLocation(Location location)
+    {
+        locationHistory.add(location);
+        return ""; // output what the new location has been set to? means changing game class (welcome() method)
+    }
 
-	public String setNewLocation(int backtrack)
-	{
-		locationHistory.add(locationHistory.get(locationHistory.size()-backtrack-1));
-		return getCurrentLocation().getLongDescription();
-	}
+    public String setNewLocation(int backtrack)
+    {
+        int backtrackIndex = locationHistory.size() - backtrack - 1;
+        
+        if(backtrackIndex < 0) {
+            return "The location you are attempting to go back to does not exist!";
+        }
+        
+        locationHistory.add(locationHistory.get(backtrackIndex));
+        return getCurrentLocation().getLongDescription();
+    }
 }
