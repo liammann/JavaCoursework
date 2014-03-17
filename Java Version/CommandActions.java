66673164759eec7b28fc239e99342a24fa3e0b5f
Thomas Punt <tpunt@hotmail.co.uk>
2014-03-17 00:42:41 +0000

    Began documenting and fixed some bugs (see details)
    > Ensured that the health potion is dropped from the player’s inventory
    after use.
    > Validated and refactored the alternate usage of the “go” command.
    > Documented the alternate “go” command.

--- Java Version/CommandActions.java
+++ Java Version/CommandActions.java
@@ -1,16 +1,32 @@
 import java.util.ArrayList;
 
+/**
+ * CommandActions is a class that delegates the entered command to invoking its
+ * respective command method.
+ * 
+ * @author (Thomas Punt) 
+ * @version (1.11)
+ */
 public class CommandActions
 {
     private CommandWords commandWords;
     private GameData gameData;
 
+    /**
+     * Constructor for CommandActions class to initialise the class fields
+     */
     public CommandActions()
     {
         commandWords = CommandWords.getInstance();
         gameData = GameData.getInstance();
     }
 
+    /**
+     * This method takes the delegated command keyword and invokes the respective function
+     * 
+     * @param  keyword   the command keyword entered
+     * @return           the return value of the invoked function
+     */
     public String invokeAction(String keyword)
     {
         String answer = "";
@@ -36,91 +52,140 @@ public class CommandActions
         return answer;
     }
 
-    public String invokeAction(String keyword, String parameter)
+    /**
+     * This method takes the delegated command keyword and the single argument associated
+     * with the command keyword, and invokes the respective function
+     * 
+     * @param  keyword     the command keyword entered
+     * @param  argument    the argument spcified with the keyword
+     * @return             the return value of the invoked function
+     */
+    public String invokeAction(String keyword, String argument)
     {
         String answer = "";
 
         switch(keyword) {
             case "go":
-                answer = go(parameter);
+                answer = go(argument);
                 break;
             case "back":
-                int param = Integer.parseInt(parameter);
-                answer = back(param);
+                answer = back(argument);
                 break;
             case "manual":
-                answer = commandWords.manual(parameter);
+                answer = commandWords.manual(argument);
                 break;
             case "use":
-                answer = use(parameter);
+                answer = use(argument);
                 break;
             case "pickup":
-                answer = pickup(parameter);
+                answer = pickup(argument);
                 break;
             case "drop":
-                answer = drop(parameter);
+                answer = drop(argument);
                 break;
             case "inspect":
-                answer = inspect(parameter);
+                answer = inspect(argument);
                 break;
             case "new":
-                answer = newGame(parameter);
+                answer = newGame(argument);
                 break;
         }
 
         return answer;
     }
 
-    public String invokeAction(String keyword, ArrayList parameters)
+    /**
+     * This method takes the delegated command keyword and the arguments associated
+     * with the command keyword, and invokes the respective function
+     * 
+     * @param  keyword     the command keyword entered
+     * @param  arguments   the arguments specified with the keyword
+     * @return             the return value of the invoked function
+     */
+    public String invokeAction(String keyword, ArrayList arguments)
     {
         String answer = "";
 
         switch(keyword) {
             case "talk":
-                answer = talk(parameters);
+                answer = talk(arguments);
                 break;
             case "go":
-                answer = go(parameters);
+                answer = go(arguments);
                 break;
             case "fight":
-                answer = fight(parameters);
+                answer = fight(arguments);
                 break;
             case "load":
-                answer = loadGame(parameters);
+                answer = loadGame(arguments);
                 break;
             case "save":
-                answer = saveGame(parameters);
+                answer = saveGame(arguments);
                 break;
         }
 
         return answer;
     }
 
+    /**
+     * This method is used to quit the game or pregame
+     * 
+     * @return     the word "quit"
+     */
     private String quit()
     {
         return "quit";
     }
 
+    /**
+     * This method is used to give help to the player upon request
+     * 
+     * @return     the task of the game and how to use its commands
+     */
     private String help()
     {
-        return getTask() + "\nType 'manual' to view your list of commands, or 'manual {command}' to learn about a specific command.";
+        return getTask() + "\nType 'manual' to view a list of commands, or 'manual {command}' to learn about a specific command.";
     }
 
+    /**
+     * This method is used to get the game's task
+     * 
+     * @return     the task of the game
+     */
     private String getTask()
     {
         return "Your task is to...";
     }
 
+    /**
+     * This method is used to update the game location by going back one location
+     * 
+     * @return     a message of the number of locations going back and the new location description
+     */
     private String back()
     {
         return "Going back 1 location: " + gameData.setNewLocation(1);
     }
 
-    private String back(int retraceSteps)
+    /**
+     * This method is used to update the game location by going back a specified number of locations
+     * 
+     * @param   retraceSteps    the number of steps to go back
+     * @return                  a message of the number of locations going back and the new location description
+     */
+    private String back(String retraceSteps)
     {
-        return "Going back " + retraceSteps + " location(s): " + gameData.setNewLocation(retraceSteps);
+        int steps = Integer.parseInt(retraceSteps);
+
+        return "Going back " + retraceSteps + " location(s): " + gameData.setNewLocation(steps);
     }
 
+    /**
+     * This method is used to pickup an object, given the object's name
+     * 
+     * @param   objectName    the name of the object to pickup
+     * @return                a message of the attempt to pick up the object
+     */
     private String pickup(String objectName)
     {
         if (gameData.getCurrentLocation().containsFixedObject(objectName)) {
@@ -131,7 +196,8 @@ public class CommandActions
             return "No such object exists in this room.";
         }
 
-        if(gameData.getPlayerObject().getInventory().addItemToInventory(gameData.getCurrentLocation().getMovableObjectByName(objectName))) {
+        MovableObject obj = gameData.getCurrentLocation().getMovableObjectByName(objectName);
+        if(gameData.getPlayerObject().getInventory().addItemToInventory(obj)) {
             gameData.getCurrentLocation().removeObject(objectName);
 
             return "You picked up " + objectName;
@@ -139,22 +205,39 @@ public class CommandActions
 
         return "You don't have enough strength to carry that!";
     }   
-    
-    private String use(String name)
-    {
-        if(gameData.getPlayerObject().getInventory().containsObject(name)) {
-            int playerHealth = gameData.getPlayerObject().getHealth();
-            int healthPotion = gameData.getPlayerObject().getInventory().getFromInventoryByName(name).getHealthPotion();
-            
-            gameData.getPlayerObject().updateHealth(playerHealth + healthPotion);
 
-            return "You added " + gameData.getPlayerObject().getInventory().getFromInventoryByName(name).getHealthPotion()
-                   + " health points \nThis gives you " + gameData.getPlayerObject().getHealth() + " health points in total";
+    /**
+     * This method is used to consume the specified potion (provided it is in their inventory)
+     * which will update the current player's health
+     * and drop the potion from the player's inventory
+     * 
+     * @param   potionName    the potion to be consumed
+     * @return                a message of the number of locations going back and the new location description
+     */
+    private String use(String potionName)
+    {
+        if(gameData.getPlayerObject().getInventory().containsObject(potionName)) {
+            int playerHealth = gameData.getPlayerObject().getHealth();
+            int healthPoints = gameData.getPlayerObject().getInventory().getFromInventoryByName(potionName).getHealthPotion();
+            
+            gameData.getPlayerObject().updateHealth(playerHealth + healthPoints);
+            gameData.getPlayerObject().getInventory().dropFromInventory(potionName);
+
+            return "You added " + healthPoints + " health points \nThis gives you "
+                   + gameData.getPlayerObject().getHealth() + " health points in total";
         }
 
         return "Use what?";
     }
 
+    /**
+     * This method is used to drop a specified MovableObject from the player's inventory
+     * (provided it is in there). It will first update the location with the dropped object,
+     * and will then remove the object from the player's inventory
+     * 
+     * @param   potionName    the name of the object to be dropped
+     * @return                a message of whether the object was dropped or not
+     */
     private String drop(String objectName)
     {
         if (!gameData.getPlayerObject().getInventory().containsObject(objectName)) {
@@ -167,19 +250,33 @@ public class CommandActions
         return "You dropped your " + objectName;
     }
 
-    private String fight(ArrayList parameters)
+    /**
+     * This method is used to check the validity of the entered fight command
+     * 
+     * @param   arguments    the list of arguments supplied with the fight keyword
+     * @return               a message that is determined upon the validity of the input command
+     */
+    private String fight(ArrayList arguments)
     {
-        if(parameters.size() != 3 || !parameters.get(1).equals("with")) {
+        if(arguments.size() != 3 || !arguments.get(1).equals("with")) {
             return "Invalid syntax used for the 'fight' command.";
         }
             
-        if(gameData.getCurrentLocation().getEnemyByName((String) parameters.get(0)).getName() ==  null) {
+        if(gameData.getCurrentLocation().getEnemyByName((String) arguments.get(0)).getName() ==  null) {
             return "The enemy entered does not exist!";
         }
        
         return "fight";
     }
-    
+
+    /**
+     * This method is used to validate the specified location direction the player would
+     * like to travel in, and then update player's location if the direction validates and
+     * the location to go to is not locked
+     * 
+     * @param   direction    the direction to travel in
+     * @return               a message that is determined upon the validity of the input command
+     */
     private String go(String direction)
     {
         if(!gameData.getCurrentLocation().isValidExit(direction)) {
@@ -192,25 +289,41 @@ public class CommandActions
 
         return updateLocation(direction);
     }
-    
-    private String go(ArrayList parameters)
-    {    
-        String direction = (String) parameters.get(0);
+
+    /**
+     * ...
+     * 
+     * @param   arguments    the arguments specified for the go keyword
+     * @return               a message that is determined upon the validity of the input command
+     */
+    private String go(ArrayList arguments) // note to self: validate the input!
+    {
+        if(arguments.size() != 2 || !arguments.get(1).equals("unlock")) {
+            return "Invalid syntax used for the 'go' command.";
+        }
+        
+        String direction = (String) arguments.get(0);
         
         if (!gameData.getCurrentLocation().isValidExit(direction)) {
             return "Invalid exit!";
         }
         
-        if(gameData.getPlayerObject().getInventory().getFromInventoryByName("key") != null) {
-            if(gameData.getPlayerObject().getInventory().getFromInventoryByName("key").getPasscode() 
-                    == gameData.getCurrentLocation().getLocationNeighour(direction).getPasscode()) {
-                gameData.getCurrentLocation().getLocationNeighour(direction).unlock();
-            }else{
-                return "That key doesn't fit that lock";
-            }             
-        } else if(gameData.getCurrentLocation().getExit(direction).isLocked()) {
-            return "That room is locked!";
+        if(!gameData.getCurrentLocation().getExit(direction).isLocked()) {
+            return "That exit is not locked!";
         }
+        
+        if(!gameData.getPlayerObject().getInventory().containsObject("key")) {
+            return "You have no key in your inventory!";
+        }
+        
+        int keyPasscode = gameData.getPlayerObject().getInventory().getFromInventoryByName("key").getPasscode();
+        int locationPasscode = gameData.getCurrentLocation().getLocationNeighour(direction).getPasscode();
+
+        if(keyPasscode != locationPasscode) {
+            return "Your key doesn't fit that lock";
+        }
+
+        gameData.getCurrentLocation().getLocationNeighour(direction).unlock();
 
         return updateLocation(direction);
     }
@@ -293,6 +406,7 @@ public class CommandActions
         
         return "The object specified does not exist!";
     }
+
     private String inventory()
     {
         return gameData.getPlayerObject().getInventory().currentInventory();
