82790876f1bd7a72ffee8a686f6df02d9214a308
Zain-Ali <up687776@myport.ac.uk>
2014-03-17 19:13:47 +0000

    Documentation
    player (finished), enemy class
    
    Change if necessary

--- Java Version/Enemy.java
+++ Java Version/Enemy.java
@@ -1,10 +1,32 @@
+/**
+* The is a  enemy class. It allow you to add name, get strength and health for the enemy
+* as well as select weapon which is inherited from inventory class. Enemy can also use move
+* able objects from selections.
+*
+* @author  (Zain Ali)
+* @version (1.1)
+*/
 public class Enemy extends Character implements java.io.Serializable
 {
+    /**
+    * This is a method which takes command from the user and invokes the function
+    * Super is a special type of keyword and it is used to access the class
+    * 
+    * @param            name  the command name entered
+    */
     public Enemy(String name)
     {
         super(name);
     }
-
+    
+    
+    /**
+    * This is a method which gives stregth to the enemy.
+    * 
+    * @param         strength command uses int data type
+    * @return        it refers to the constructor of the same class where I am writing this code. It also means that it returns the enemy's strength
+    *
+    */
     public Enemy hasStrength(int strength)
     {
         this.strength = strength;
@@ -12,17 +34,35 @@ public class Enemy extends Character implements java.io.Serializable
         return this;
     }
 
+    /**
+    * This is a method which gives health to the enemy.
+    * 
+    * @param         health command uses int data type to provide health to the enemy
+    * @return        it refers to the constructor of the same class where I am writing this code. It also means that it returns the enemy's health
+    */
     public Enemy withHealth(int health)
     {
         this.health = health;
         return this;
     }
-
+    
+    /**
+    * This method puts new weapons into the enemy's inventry class
+    * 
+    * @param         Its the moveable object
+    * @return        it refers to the constructor of the same class where I am writing this code. It also means that it returns the enemy's weapons.
+    */
     public Enemy andHasWeapon(MovableObject weapon)
     {
         this.getInventory().addItemToInventory(weapon);
         return this;
     }
+    
+    /**
+    * This method puts new object into the enemy's inventry class
+    * @param         Its the moveable object
+    * @return        it refers to the constructor of the same class where I am writing this code. It also means that it returns the enemy's weapons.
+    */
     public Enemy andHasObject(MovableObject object)
     {
         this.getInventory().addItemToInventory(object);
--- Java Version/Player.java
+++ Java Version/Player.java
@@ -1,15 +1,40 @@
+
+/**
+* The is a  Player class. It allow you to add name, get strength and health for the player
+* as well as select weapon which is inherited from inventory class.
+*
+* @author  (Zain Ali)
+* @version (1.1)
+*/
 public class Player extends Character implements java.io.Serializable
 {
+    /**
+     * This is a method which takes command from the user and invokes the function
+     * 
+     * @param       name  the command name entered
+     * @return      the return value of the invoked function
+     */
     public static Player create(String name)
     {
         return new Player(name);
     }
 
+    /**
+    * This is a constructor. Super is a special type of keyword and it is used to access the class
+    * 
+    * @param         name  the command name entered
+    */
     private Player(String name)
     {
         super(name);
     }
 
+    /**
+    * This is a method which gives strength to player.
+    * 
+    * @param        strength command uses int data type
+    * @return       it refers to the constructor of the same class where I am writing this code.
+    */
     public Player hasStrength(int strength)
     {
         this.strength = strength;
@@ -17,6 +42,12 @@ public class Player extends Character implements java.io.Serializable
         return this;
     }
 
+    /**
+    * This is a method which shows that the player uses health
+    * 
+    * @param         health command uses int data type
+    * @ return       it refers to the constructor of the same class where I am writing this code.
+    */
     public Player withHealth(int health)
     {
         this.health = health;
@@ -24,6 +55,12 @@ public class Player extends Character implements java.io.Serializable
         return this;
     }
 
+        /**
+    * This is method is inherit from Inventory class where it allow the player to get the weapons.
+    * 
+    * @param         it uses MovableObject weapons
+    * @ return       it refers to the constructor of the same class where I am writing this code.
+    */
     public Player andHasWeapon(MovableObject weapon)
     {
         getInventory().addItemToInventory(weapon);
