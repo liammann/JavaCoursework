524113bb12d8c945f66101ed193a4e49e4f2790b
JozefStodulski <jozefstodulski@gmail.com>
2014-03-17 18:33:28 +0000

    doc invent

--- Java Version/Inventory.java
+++ Java Version/Inventory.java
@@ -7,7 +7,7 @@ public class Inventory implements java.io.Serializable
     private int inventoryWeight = 0;
     private int inventoryWeightCapacity = 10;
     private HashMap<String, MovableObject> inventory;
-
+    
     public Inventory()
     {
         inventory = new HashMap<String, MovableObject>();
@@ -17,6 +17,12 @@ public class Inventory implements java.io.Serializable
         return inventory;
     }
 
+    /**
+     * This method adds an object with its name to the inventory hashmap
+     * 
+     * @param  MovableObject  the object to be added
+     * @return  Boolean boolean of whether movableobject was added or not
+     */
     public boolean addItemToInventory(MovableObject item)
     { // ADD ITEMS TO ENEMIES SUCH AS KEYS 
         
@@ -28,24 +34,46 @@ public class Inventory implements java.io.Serializable
         return true;
     }
     
+    /**
+     * This method removes an object from the inventory
+     * 
+     * @param  String  the name of the object and name to be removed
+     */
     public void dropFromInventory(String objectName)
     {
         inventoryWeight -= getFromInventoryByName(objectName).getWeight();
         inventory.remove(objectName);
     }
-   
-        
+    
+    /**
+     * This method removes an object from the inventory
+     * 
+     * @param  MovableObject  the object to be removed with its name
+     */
     public void dropFromInventoryByObject(MovableObject object)
     {
         inventoryWeight -= object.getWeight();
         inventory.remove(object.getObjectName());
     }
     
+    /**
+     * This method returns the object connected to its given name
+     * 
+     * @param   String  the name of the needed object
+     * @return  MovableObject   the needed object
+     */
     public MovableObject getFromInventoryByName(String objectName)
     {
         return inventory.get(objectName);
     }
     
+    /**
+     * This method searches the inventory for a movable object key with a particular keycode
+     * 
+     * @param   int  the keycode of the needed object
+     * @return  MovableObject   the needed object
+     * @return  null
+     */
     public MovableObject getFromInventoryByNamePass(int passcode)
     {
 
@@ -57,11 +85,22 @@ public class Inventory implements java.io.Serializable
        return null;
     }
     
+    /**
+     * This method checks the avaiability of an object in the inventory
+     * 
+     * @param   String  the name of the queried object
+     * @return  Boolean   boolean of whether the object is in the inventory
+     */
     public boolean containsObject(String name)
     {
         return inventory.containsKey(name);
     }
-
+    
+    /**
+     * This method compiles a list of the names of all objects in the inventory
+     * 
+     * @return  String   a list of everything in the inventory
+     */
     public String currentInventory()
     {
         String returntxt = "You currently have:\n";
@@ -72,12 +111,23 @@ public class Inventory implements java.io.Serializable
 
         return returntxt;
     }
-
+    
+    /**
+     * This method counts the number of objects in the inventory
+     * 
+     * @return  int the number of objects in the inventory
+     */
     public int inventorySize()
     {
         return inventory.size();
     }
-
+    
+    /**
+     * This method returns the object connected to its given name
+     * 
+     * @param   String  the name of the needed object
+     * @return  MovableObject   the needed object
+     */
     public int inventoryWeight()
     {
         for(MovableObject item: inventory.values()) {
